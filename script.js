97aeb825b18a0ada1c23c3904c9f868c457bb6d7
Miguel Gomez <miguelgomezca2021@gmail.com>
2025-02-08 23:37:49 -0500

    Pushing maxArea 2/8

--- script.js
+++ script.js
@@ -732,4 +732,22 @@ function decode(str) {
 // }
 
 console.log(decode(test))
+console.log('-------------------------------')
 
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    for(let i = 0; i < height.length; i++){
+      const area = Math.min(height[left], height[right]) * (right - left);
+      maxArea = Math.max(maxArea, area);
+      if(height[left] < height[right]){
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return maxArea;
+};
+
+console.log(maxArea(179))
